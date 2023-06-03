4bb0defe3aa2ade7b0731ffee72c4b29f36af3d0
valeria-markish <84080359+valeria-markish@users.noreply.github.com>
2023-06-03 13:23:24 +0200

    Added open-source link

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Valeria Markish and is{" "}
+        <a
+          href="https://github.com/valeria-markish/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
