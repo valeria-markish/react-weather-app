4c887a56b26b315d9937333c3f720b638f9aa7e7
valeria-markish <84080359+valeria-markish@users.noreply.github.com>
2023-10-27 13:10:24 +0200

    updated bootstrap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Valeria Markish and is{" "}
-        <a
-          href="https://github.com/valeria-markish/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Valeria Markish and is{" "}
+          <a
+            href="https://github.com/valeria-markish/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
